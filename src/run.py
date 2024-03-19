fe56a7c9242a3d1a19e04874a0d04eff43acc93b
s27559 <161855502+s27559@users.noreply.github.com>
2024-03-19 11:24:19 +0100

    abstract class added [finish ex10]

--- src/run.py
+++ src/run.py
@@ -1,10 +1,10 @@
 import square_generator
 
 try:
-    square = square_generator.SquareGenerator(1,10)
+    #square = square_generator.SquareGenerator(1,10)
     cubes = square_generator.CubicGenerator(1,5)
-    print(square.squares_in_range())
-    print(cubes.cubes_in_range())
-    print(cubes.squares_in_range())
+    #print(square.squares_in_range())
+    #print(cubes.cubes_in_range())
+    print(cubes.generate_squares())
 except ValueError:
     print("Invalid range, start > end")
--- src/square_generator/__init__.py
+++ src/square_generator/__init__.py
@@ -1,18 +1,23 @@
 import math
+from abc import ABC, abstractmethod
 
-class SquareGenerator:
+class SquareGenerator(ABC):
+    @abstractmethod
+    def generate_squares(self):
+        pass
+
+class CubicGenerator(SquareGenerator):
     
     def __init__(self, start, end):
         self.start = start
         self.end = end
     
-    def squares_in_range(self) -> list:
-        if self.start > self.end:
-            raise ValueError
-        squares = [math.pow(x,2) for x in range(self.start, self.end+1)]
-        return squares
-
-class CubicGenerator(SquareGenerator):
+    def generate_squares(self):
+        if self.start < self.end:
+            squares = [math.pow(x,2) for x in range(self.start, self.end+1)]
+            return squares
+        else:
+            return ValueError
 
     def squares_in_range(self) -> list:
         if self.start < self.end:
